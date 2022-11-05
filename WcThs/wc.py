bdd8228d26790ccd0c34dcef086ecde062a52288
jianwei.lv <lvjianwei218@163.com>
2022-11-06 00:12:00 +0800

    1

--- WcThs/wc.py
+++ WcThs/wc.py
@@ -7,9 +7,13 @@ import os
 
 name = "wc"
 
-res = pywencai.getWencai(question='两连阴行业板块；1天前涨幅；2日涨幅', secondary_intent='zhishu', start_time=2022-10-10)
+# 获取数据
+res = pywencai.getWencai(question='两连阴概念板块；2022.11.03涨幅；2日涨幅；2022.11.03换手率; 2022.11.04涨幅; 2022.11.04换手率',
+                         secondary_intent='zhishu', start_time=2022-10-10)
+result = pd.DataFrame(res)
+# 截取需要的列
+solve_result = res[['code', '指数简称', '指数@换手率[20221103]', '指数@换手率[20221104]',
+                    '指数@涨跌幅:前复权[20221103]', '指数@涨跌幅:前复权[20221104]', '指数@区间涨跌幅:不复权[20221103-20221104]']]
 
-
-result = pd.DataFrame(res, columns=res.columns)
-result.to_csv(os.path.join(path.CSV_PATH, name + '.xls'), encoding="utf-8", index=False)
-print("to_excel success to {}".format(os.path.join(path.CSV_PATH, name + '.csv')))
+solve_result.to_excel(os.path.join(path.CSV_PATH, name + '.xls'), encoding="utf-8", index=False)
+print("to_excel success to {}".format(os.path.join(path.CSV_PATH, name + '.xls')))
